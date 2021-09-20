aced9b4268fd9c79141482c144686ddfef0e5848
Brandon Ashton <ashtonbt1@gmail.com>
2021-09-20 16:37:15 -0500

    Small edit to homebaked Card class

--- low_level_2021_09/oop_01/classes/card.py
+++ low_level_2021_09/oop_01/classes/card.py
@@ -2,6 +2,9 @@ from datetime import datetime
 from .validation import D20, OneOf, Number, String
 from .reference_lists import CARD_TEAMS
 
+# Class references
+from .player import Player
+
 class Card:
     # Define validations using the Validator classes
     first_name = String(minsize=1, maxsize=100)
@@ -10,9 +13,10 @@ class Card:
     team = OneOf(*CARD_TEAMS)
     steal = D20()
 
-    def __init__(self, first_name, last_name, year, team, steal):
+    def __init__(self, first_name, last_name, year, team, steal, player):
         self.first_name = first_name
         self.last_name = last_name
         self.year = year
         self.team = team
         self.steal = steal
+        self.player = player
