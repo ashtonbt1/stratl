97485937b432c01eca0342b70ea6db066c8e07bd
Brandon Ashton <ashtonbt1@gmail.com>
2021-09-20 19:22:34 -0500

    First base template

--- django/league/views.py
+++ django/league/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Player
 
 # Create your views here.
+def player_list(request):
+    players = Player.objects.all()
+    return render(request, 'league/player_list.html', {'players': players})
